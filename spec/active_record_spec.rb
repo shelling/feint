a9abda1212f980619f46c975301d018f79b9230e
shelling <navyblueshellingford@gmail.com>
2016-12-27 15:22:11 +0800

    Can create ActiveRecord instances

--- spec/active_record_spec.rb
+++ spec/active_record_spec.rb
@@ -1,9 +1,20 @@
 require "spec_helper"
-require 'active_record'
+require "active_record"
 
 class Product < ActiveRecord::Base
+  establish_connection(adapter:  "sqlite3", database: "feint.db")
+  connection.create_table table_name, force: true do |t|
+    t.string  :name
+    t.text    :description
+  end
 end
 
 describe Product do
-  it "should initialize ActiveRecord instance"
+  it "could initialize and save a ActiveRecord instance" do
+    expect {
+      Product.create
+    }.to change {
+      Product.count
+    }.by(1)
+  end
 end
