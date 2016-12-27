9c92e8675fbf1eef28c42b49a7f35ee958e2cfd5
shelling <navyblueshellingford@gmail.com>
2016-12-27 18:15:47 +0800

    Provide fundamental mechanisms of a presenter
    - Could list attributes to show directly
    - Could transform attributes to new names

--- lib/feint/presenter.rb
+++ lib/feint/presenter.rb
@@ -1,4 +1,28 @@
 module Feint
   class Presenter
+    def initialize(model) # :nodoc:
+      @model = model
+    end
+
+    class << self
+      # Takes a list of columns
+      def attributes(*columns)
+        columns.each do |column|
+          define_method column do
+            @model.send(column)
+          end
+        end
+      end
+
+      # Take a list of mappiings
+      # { old_column_name => new_column_name }
+      def transform(mappings)
+        mappings.each do |old, new|
+          define_method new do
+            @model.send(old)
+          end
+        end
+      end
+    end
   end
 end
--- spec/presenter_spec.rb
+++ spec/presenter_spec.rb
@@ -1,5 +1,34 @@
 require "spec_helper"
+require "active_record"
+
+class User < ActiveRecord::Base
+  establish_connection(adapter: "sqlite3", database: "feint.db")
+  connection.create_table table_name, force: true do |t|
+    t.string  :name
+    t.integer :age
+  end
+end
+
+class UserPresenter < Feint::Presenter
+  attributes :name, :age
+end
+
+class UserTransformer < Feint::Presenter
+  transform name: :handle, age: :year
+end
 
 describe Feint::Presenter do
-  it "converts attributes"
+  let(:user) { User.create(name: "shelling", age: 30) }
+
+  it "takes present attributes" do
+    user_presenter = UserPresenter.new(user)
+    expect(user_presenter.name).to eq(user.name)
+    expect(user_presenter.age).to  eq(user.age)
+  end
+
+  it "transforms attributes" do
+    user_transformer = UserTransformer.new(user)
+    expect(user_transformer.handle).to eq(user.name)
+    expect(user_transformer.year).to   eq(user.age)
+  end
 end
