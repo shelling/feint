784e16815cfaf8b478a7c51fa2551d6be19ba6b5
shelling <navyblueshellingford@gmail.com>
2016-12-28 16:45:26 +0800

    Add attribute macro
    - Type Coercion
    - Transformation

--- spec/presenter_spec.rb
+++ spec/presenter_spec.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   establish_connection(adapter: "sqlite3", database: "feint.db")
   connection.create_table table_name, force: true do |t|
     t.string  :name
+    t.string  :email
     t.integer :age
   end
 end
@@ -15,6 +16,12 @@ end
 
 class UserTransformer < Feint::Presenter
   transform name: :handle, age: :year
+end
+
+class UserConstraintPresenter < Feint::Presenter
+  attribute :name, key: :handle
+  attribute :email
+  attribute :age, type: String
 end
 
 describe Feint::Presenter do
@@ -30,5 +37,22 @@ describe Feint::Presenter do
     user_transformer = UserTransformer.new(user)
     expect(user_transformer.handle).to eq(user.name)
     expect(user_transformer.year).to   eq(user.age)
+  end
+
+  it "can declare single field" do
+    user_constraint_presenter = UserConstraintPresenter.new(user)
+    expect(user_constraint_presenter.handle).to eq(user.name)
+  end
+
+  it "can declare single field and guess field name" do
+    user_constraint_presenter = UserConstraintPresenter.new(user)
+    expect(user_constraint_presenter.email).to eq(user.email)
+  end
+
+  it "can declare single field and check its type" do
+    user_constraint_presenter = UserConstraintPresenter.new(user)
+    expect {
+      user_constraint_presenter.age
+    }.to raise_error(TypeError, "age should be String and is Fixnum")
   end
 end
